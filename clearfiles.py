2ee2763b664fcf61e65aaa3f25e578b86694b10f
Prospect Project <benjamin.w.massey@gmail.com>
2020-06-03 04:03:39 -0700

    Fixed path on clearfiles.py

--- clearfiles.py
+++ clearfiles.py
@@ -5,7 +5,7 @@ i =  1
 
 while i <= number:
 	for type in ["P", "M"]:
-		mfile = open("./Results/" + type + str(i) + ".txt", "w")
+		mfile = open("./results/" + type + str(i) + ".txt", "w")
 		mfile.write("")
 		mfile.close()
 	i += 1

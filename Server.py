0ce66d16dc04d4d9e88f6eb716e2b8128d579539
Prospect Project <prospect@raspberrypi>
2020-06-03 03:45:18 -0700

    Added start and end pages

--- Server.py
+++ Server.py
@@ -5,30 +5,30 @@ app = Flask(__name__)
 number_of_questions = 8
 
 @app.route('/')
-def M1():
+def index():
 	rand = random.randint(1, 2)
 	if rand == 1:
 		print("User given money version")
-		return render_template("M1.html")
+		return render_template("Mstart.html")
 	else:
 		print("User given points version")
-		return render_template("P1.html")
+		return render_template("Pstart.html")
 
-@app.route('/<type>/<number>/<answer>')
-def serve(type, number, answer):
-	assert type in ["P", "M"]
+@app.route('/<version>/<number>/<answer>')
+def serve(version, number, answer):
+	assert version in ["P", "M"]
 	assert int(number) <= (number_of_questions + 1)
 	assert int(number) > 0
 	assert answer in ["A", "B"]
-	filename = "./results/" + str(type) + str(int(number) - 1) + ".txt"
+	filename = "./results/" + str(version) + str(int(number) - 1) + ".txt"
 	print("Writing answer", answer, "to", filename)
 	file = open(filename, "a")
 	datum = str(answer) + "\n"
 	file.write(datum)
 	file.close()
 	if int(number) == (number_of_questions + 1):
-		return "done :)"
-	page = str(type) + str(number) + ".html"
+		return render_template("end.html")
+	page = str(version) + str(number) + ".html"
 	print("Serving page", page)
 	return render_template(page)
 

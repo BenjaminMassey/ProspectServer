ba7cb7c88e7a30e123ad390ebac67166554ebdcf
root <root@raspberrypi>
2020-06-09 04:08:42 -0700

    working ip getter

--- Server.py
+++ Server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import random
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def IPcompletedQuestion(filename, ip):
     f = open(filename, "r")
     contents = str(f.read())
     f.close()
-    return str(ip) in contents
+    return ip in contents
 
 @app.route('/HackMainframe')
 def easterEgg():
@@ -28,15 +28,17 @@ def index():
         print("User given points version")
         return render_template("Pstart.html")
 
-@app.route('/<version>/<number>/<answer>')
+@app.route('/<version>/<number>/<answer>', methods=["GET"])
 def serve(version, number, answer):
     assert version in ["P", "M"]
     assert int(number) <= (number_of_questions + 1)
     assert int(number) > 0
     assert answer in ["A", "B", "X"]
     if answer is not "X":
+        #ip_addr = str(jsonify({'ip': request.remote_addr}))
+        ip_addr = str(request.headers.getlist("X-Forwarded-For")[0])
         ip_file = "./ip/" + str(version) + str(int(number) - 1) + "ip.txt"
-        repeat_user = IPcompletedQuestion(ip_file, request.remote_addr)
+        repeat_user = IPcompletedQuestion(ip_file, ip_addr)
         if not repeat_user:
             filename = "./results/" + str(version) + str(int(number) - 1) + ".txt"
             print("Writing answer", answer, "to", filename)
@@ -45,8 +47,8 @@ def serve(version, number, answer):
             file.write(datum)
             file.close()
             ipfile = open(ip_file, "a")
-            print("adding ip", str(request.remote_addr))
-            ipfile.write(str(request.remote_addr) + "\n")
+            print("adding ip", ip_addr)
+            ipfile.write(ip_addr + "\n")
             ipfile.close()
     if int(number) == (number_of_questions + 1):
         return render_template("end.html")
